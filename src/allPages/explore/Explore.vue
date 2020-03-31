4058ed4498900ae91948b989f10691cb2357abb3
lxp0214 <1320063725@qq.com>
2020-03-31 16:54:43 +0800

    add reset.css

---
+++ src/allPages/explore/Explore.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>explore</div>
+</template>
+
+<script>
+export default {
+    name: 'Explore'
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/allPages/Login'
+import Explore from '@/allPages/explore/Explore'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },{
+      path: '/explore',
+      name: 'Explore',
+      component: Explore
     }
+
   ]
 })

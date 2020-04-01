ba2ee690ac2b475f8e77ced2d02638b7926e2eb2
AnbolKing <272344341@qq.com>
2020-04-01 15:40:24 +0800

    wza0401@2

--- src/router/index.js
+++ src/router/index.js
@@ -5,32 +5,36 @@ import Explore from '@/allPages/explore/Explore'
 import Myself from '@/allPages/myself/Myself'
 import MyselfFocus from '@/allPages/myself/myself-pages/myself-focus/MyselfFocus'
 import MyselfCollections from '@/allPages/myself/myself-pages/myself-collections/MyselfCollections'
+import MyseflAbout from '@/allPages/myself/myself-pages/myself-information/myself-information-components/About'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: Login
-    },{
-      path: '/explore',
-      name: 'Explore',
-      component: Explore
-    },{
-      path: '/myself',
-      name: 'Myself',
-      component: Myself
-    },{ 
-      path: '/focus',
-      name: 'MyselfFocus',
-      component: MyselfFocus
-    }, {
-    path: '/collections',
-    name: 'MyselfCollections',
-    component: MyselfCollections
-    }
+    routes: [{
+            path: '/',
+            name: 'Login',
+            component: Login
+        }, {
+            path: '/explore',
+            name: 'Explore',
+            component: Explore
+        }, {
+            path: '/myself',
+            name: 'Myself',
+            component: Myself
+        }, {
+            path: '/focus',
+            name: 'MyselfFocus',
+            component: MyselfFocus
+        }, {
+            path: '/collections',
+            name: 'MyselfCollections',
+            component: MyselfCollections
+        }, {
+            path: '/about',
+            name: 'MyseflAbout',
+            component: MyseflAbout
+        }
 
-  ]
+    ]
 })

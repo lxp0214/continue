7438004c55394a84d3fa439bd219497723a2349d
AnbolKing <272344341@qq.com>
2020-04-01 11:25:58 +0800

    wza0401

---
+++ src/allPages/myself/myself-pages/myself-collections/myselfCollections.vue
@@ -0,0 +1,24 @@
+<template>
+     <div class="myselfCollections">
+         <collections-header></collections-header>
+         <collections-list></collections-list>
+     </div>
+</template>
+
+<script>
+import axios from 'axios'
+import CollectionsHeader from './myself-collections-components/Header'
+import CollectionsList from './myself-collections-components/List'
+export default {
+  name: 'MyselfCollections',
+  components: {
+      CollectionsHeader:CollectionsHeader,
+      CollectionsList:CollectionsList
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='stylus'>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,23 +3,26 @@ import Router from 'vue-router'
 import Login from '@/allPages/Login'
 import Explore from '@/allPages/explore/Explore'
 import MyselfFocus from '@/allPages/myself/myself-pages/myself-focus/MyselfFocus'
+import MyselfCollections from '@/allPages/myself/myself-pages/myself-collections/MyselfCollections'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-            path: '/',
-            name: 'Login',
-            component: Login
-        }, {
-            path: '/explore',
-            name: 'Explore',
-            component: Explore
-        }, {
-            path: '/focus',
-            name: 'MyselfFocus',
-            component: MyselfFocus
-        }
-
-    ]
+        path: '/',
+        name: 'Login',
+        component: Login
+    }, {
+        path: '/explore',
+        name: 'Explore',
+        component: Explore
+    }, {
+        path: '/focus',
+        name: 'MyselfFocus',
+        component: MyselfFocus
+    }, {
+        path: '/collections',
+        name: 'MyselfCollections',
+        component: MyselfCollections
+    }]
 })
